95e51eddef72fbee9d25992d5c29403ffd7eef89
Rick <panruru1224@hotmail.com>
2017-11-23 18:07:35 +0800

    Playable interface implementation

--- play.py
+++ play.py
@@ -1,6 +1,35 @@
-import sys
-sys.path.append('/home/rick/project/pygo')
-import pygo
+import gogame
+import agent
 
-env = pygo.Env()
+boardsize = 9
+
+game = gogame.GoGame(boardsize)
+
+agent = agent.Agent()
+
+interactive = False
+
+games_to_play = 100
+
+while True:
+
+    if interactive:
+        vertex = raw_input('%s plays:'%game.current_color)
+        if vertex == 'agent':
+            vertex = agent.play(game.current_color,game.legal_states)
+    else:
+        vertex = agent.play(game.current_color,game.legal_states)
+    game.play(vertex) 
+
+    if game.end:
+        if interactive:
+            play_more = raw_input('Play more?')
+            if play_more == 'y':
+                game.reset()
+            else:
+                break
+        else:
+            game.save()
+            games_to_play -= 1
+            game.reset()
 
