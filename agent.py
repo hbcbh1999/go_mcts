906bae693bf0d2f1a845c6f766b305620c0afa84
Rick <panruru1224@hotmail.com>
2017-12-21 13:45:45 +0800

    Fix random action

--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
 from model.model import Model
 import tensorflow as tf
 import numpy as np
-
+import random
 
 class Agent:
     def __init__(self,eps):
@@ -13,30 +13,34 @@ class Agent:
         _result = self.model.inference(self.sess,batch)
         return _result
     def play(self,color,states):
-        _vertex = []
-        _states = []
-
-        for k,v in states.items():
-            _vertex.append(k)
-            _states.append(v)
-      
-        _states = np.array(_states)
-        _states.shape += (1,)
 #        _b_cap = np.array([s[1][1] for s in states])
 #        _b_cap.shape += (1,)
 #        _w_cap = np.array([s[1][2] for s in states])
 #        _w_cap.shape += (1,)
 
 #        batch = [_states,_b_cap,_w_cap]
-        batch = [_states]
 
-        if np.random.random() < self.eps:
-            idx = np.random.randint(_states.shape[0])
+        if random.random() < self.eps:
+            return random.choice(states.keys())
         else:
+            _vertex = []
+            _states = []
+
+            for k, v in states.items():
+                _vertex.append(k)
+                _states.append(v)
+
+            _states = np.array(_states)
+            _states.shape += (1,)
+
+            batch = [_states]
+
             _result = self.evaluate(batch)
+
             if color == 'black':
                 idx = np.argmax(_result)
             else:
                 idx = np.argmin(_result)
-        return _vertex[idx]
+
+            return _vertex[idx]
 
