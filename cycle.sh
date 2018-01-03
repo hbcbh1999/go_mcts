de6e411b0b0a0c6dccf8762f70dd3acce72a6a49
Rick <panruru1224@hotmail.com>
2018-01-03 09:59:57 +0800

    Now uses python3

--- cycle.sh
+++ cycle.sh
@@ -9,23 +9,21 @@ rm -r data/self*
 rm -r saved_model
 
 
+ngames=5
 for ((x=0; x<200; x++))
 {
 	echo Cycle ${x} 
 	for ((i=1; i<=4; i++)){
 		mkdir -p data/self${i}
+		python3 play.py --cycle ${x} --black agent --white agent --ngames ${ngames} --save --save_dir data/self${i}/ >> logs/log_${i} &
 	}
-	python play.py --cycle ${x} --black agent --white agent --ngames 250 --save --save_dir data/self1 >> logs/log_1 &
-	python play.py --cycle ${x} --black agent --white agent --ngames 250 --save --save_dir data/self2 >> logs/log_2 &
-	python play.py --cycle ${x} --black agent --white random --ngames 250 --save --save_dir data/self3 >> logs/log_3 &
-	python play.py --cycle ${x} --black random --white agent --ngames 250 --save --save_dir data/self4 >> logs/log_4 &
 
 	wait
 
-	python train.py --new --epochs 10 --data_dir data/self1 data/self2 data/self3 data/self4 >> logs/log_train
+	python3 train.py --new --epochs 10 --data_dir data/self1 data/self2 data/self3 data/self4 >> logs/log_train
 
-	python play.py --black agent --white random --ngames 100 >> logs/log_bench_black &
-	python play.py --black random --white agent --ngames 100 >> logs/log_bench_white &
+	python3 play.py --black agent --white random --ngames 100 >> logs/log_bench_black &
+	python3 play.py --black random --white agent --ngames 100 >> logs/log_bench_white &
 
 	mkdir -p data/cycle${x}
 	mv data/self* data/cycle${x}
